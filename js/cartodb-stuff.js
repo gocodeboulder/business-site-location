90dd90f856ae828c1b6e6a4894472b1ca03316b1
Will Klein <will.klein@gmail.com>
2014-03-23 12:53:24 -0600

    Add avatar modal.

--- js/cartodb-stuff.js
+++ js/cartodb-stuff.js
@@ -88,4 +88,14 @@ $(document).ready(function() {
       }
     });
   });
+
+  $('#avatarModal .btn').click(function() {
+    var id = this.id;
+    var text = $(this).attr('data-text');
+
+    text = text ? 'You may be interested in: ' + text : '';
+
+    $('#avatarDescription').text(text);
+  });
+
 });
