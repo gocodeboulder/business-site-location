26b952cff26a35695ad33bde8b2267f3a9466d37
Willy Douglas <willygdouglas@gmail.com>
2014-03-22 20:11:08 -0600

    Changed cartodb to Scotts data how it is handled.

--- js/cartodb-stuff.js
+++ js/cartodb-stuff.js
@@ -1,40 +1,28 @@
-function main() {
-  var map = new L.Map('map', {
-    zoomControl: false,
-    center: [39, -105.5],
-    zoom: 7
-  });
+var mapLayers = { };
 
-  L.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
-    attribution: 'Stamen'
-  }).addTo(map);
+$(document).ready(function() {
+  var url = 'http://sebrenner.cartodb.com/api/v2/viz/0ea1f37e-b21e-11e3-bf2d-0edbca4b5057/viz.json';
+  var me = this;
 
-  cartodb.createLayer(map, 'http://douglas.cartodb.com/api/v2/viz/510c38ec-b1e6-11e3-b882-0e73339ffa50/viz.json')
-   .addTo(map)
-   .on('done', function(layer) {
+  console.log('ready');
 
-    layer.setInteraction(true);
+  cartodb.createVis('map', url, {center: [39, -105.5], zoom: 7})
+  .done(function(vis, layers) {
+    console.log('layers: ');
+    console.dir(layers[1].getSubLayer(0));
+    mapLayers.layer0 = layers[1].getSubLayer(0);
+    console.log(mapLayers.layer0.getSQL());
 
-    layer.on('featureOver', function(e, pos, latlng, data) {
-      cartodb.log.log(e, pos, latlng, data);
+    // Sidebar controls
+    $("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("active");
     });
 
-    layer.on('error', function(err) {
-      cartodb.log.log('error: ' + err);
-    });
-  }).on('error', function() {
-    cartodb.log.log("some error occurred");
+    $('.slider').slider()
+      .on('slide', function (e) {
+        //console.log('Changed ' + $(this).id + ' val ' + e.value);
+
+      });
   });
-
-
-  // Sidebar controls
-  $("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("active");
-  });
-  
-  $('.slider').slider();
-}
-
-// you could use $(window).load(main);
-window.onload = main;
+});
