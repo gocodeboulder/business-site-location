b9789cb2899fc1c6690bce10c164ce04e1e75b11
Willy Douglas <willygdouglas@gmail.com>
2014-03-22 21:28:33 -0600

    Add CartoCSS adjust to sliders.

--- js/cartodb-stuff.js
+++ js/cartodb-stuff.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
     console.log('layers: ');
     console.dir(layers[1].getSubLayer(0));
     mapLayers.layer0 = layers[1].getSubLayer(0);
+    mapLayers.layer1 = layers[1].getSubLayer(1);
     console.log(mapLayers.layer0.getSQL());
 
     // Sidebar controls
@@ -19,10 +20,30 @@ $(document).ready(function() {
       $("#wrapper").toggleClass("active");
     });
 
-    $('.slider').slider()
+    $('.slider').slider({'min': 0, 'max': 1, step: '0.1'})
       .on('slide', function (e) {
         //console.log('Changed ' + $(this).id + ' val ' + e.value);
+        var sliderID = $( this ).attr('id');
+        //console.log(sliderID);
+        switch(sliderID)
+        {
+          case 'slider1':
+            mapLayers.layer0.setCartoCSS(
+              '#the_geom_webmercator { opacity: '+ e.value +'; }'
+            );
+            console.log('layer0 cartoCSS changed');
+            break;
+          case 'slider2':
 
+            mapLayers.layer1.setCartoCSS(
+              '#the_geom_webmercator { opacity: '+ e.value +'; }'
+            );
+            console.log('CSS: ' + mapLayers.layer1.getCartoCSS() );
+            console.log('layer1 cartoCSS changed');
+            break;
+          default:
+            break;
+        }
       });
   });
 });
